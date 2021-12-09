bb6be19e7eb9ccfa5a89c2f3152d30ec6625d944
Roman <Roman444.zaharov@gmail.com>
2021-12-09 17:32:40 +0300

    Управление секундомером

---
+++ app/src/main/java/com/example/stopwatch/StopwatchStateHolder.kt
@@ -0,0 +1,38 @@
+package com.example.stopwatch
+
+class StopwatchStateHolder(
+    private val stopwatchStateCalculator: StopwatchStateCalculator,
+    private val elapsedTimeCalculator: ElapsedTimeCalculator,
+    private val timeStampMillisecondsFormatter: TimeStampMillisecondsFormatter
+) {
+    var currentState: StopwatchState = StopwatchState.Paused(stopStateTime)
+
+    fun start(){
+        currentState = stopwatchStateCalculator.calculateRunningState(currentState)
+    }
+
+    fun pause(){
+        currentState = stopwatchStateCalculator.calculatePausedState(currentState)
+    }
+
+    fun stop(){
+        currentState = StopwatchState.Paused(stopStateTime)
+    }
+
+    companion object{
+        private const val stopStateTime = 0L
+    }
+
+    fun getStringTimeRepresentation(): String {
+        val elapsedTime = when (val currentState = currentState) {
+            is StopwatchState.Running -> {
+                elapsedTimeCalculator.calculate(currentState)
+            }
+            is StopwatchState.Paused -> {
+                currentState.elapsedTime
+            }
+        }
+        return timeStampMillisecondsFormatter.format(elapsedTime)
+    }
+
+}
